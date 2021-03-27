eb19522597d13c1273f6c3f39383ba0138cbded4
mizcaroll <carolomukule@gmail.com>
2021-03-27 12:03:08 +0300

    finding the first letter of a string

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-countString = 0;
-myString = "What a fine day for JS"
+//using bracket notation to find the first letter of a string
+var firstLetter = "";
+var myName = "Carol";
+
+firstLetter = myName[0];
+console.log(firstLetter);
+
+/* //finding the length of a string
+var countString = 0;
+var myString = "What a fine day for JS"
 
 countString = myString.length;
-console.log(countString);
+console.log(countString); */
