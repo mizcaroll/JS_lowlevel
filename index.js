a9e7155630682295176de0ebc9a975dbce7ddeb3
mizcaroll <carolomukule@gmail.com>
2021-03-27 12:31:39 +0300

    finding the nth to last character of a string

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-//using bracket notation to find the first letter of a string
+//using bracket notation to find the nth to last character of a string based on its length
+var newName = "Mbithe";
+var lastCharacter = newName[newName.length - 3]; //translates to newName[3] i.e 't'
+console.log(lastCharacter);
+
+/* //using bracket notation to find the last character of a string based on its length
+
+var newName = "Mbithe";
+var lastCharacter = newName[newName.length - 1]; //translates to newName[5]
+console.log(lastCharacter); */
+
+/* //using bracket notation to find the nth character of a string
+var nthLetter = "";
+var newString = "What should i have for lunch?";
+
+nthLetter = newString[10]; //note that JS index starts from 0.
+console.log(nthLetter); */
+
+/* //using bracket notation to find the first letter of a string
 var firstLetter = "";
 var myName = "Carol";
 
 firstLetter = myName[0];
 console.log(firstLetter);
+ */
 
 /* //finding the length of a string
 var countString = 0;
