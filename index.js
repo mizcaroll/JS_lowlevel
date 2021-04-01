68ac2a2f9d0b9aefd1f1269a4ee2217a910186c5
mizcaroll <carolomukule@gmail.com>
2021-04-01 11:48:20 +0300

    record collection challenge

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-//accessing nested arrays
+//update record collection exercise
+
+/* You are given a JSON object representing a part of your musical album collection. Each album has a unique id number as its key and several other properties. Not all albums have complete information.
+You start with an updateRecords function that takes an object like collection, an id, a prop (like artist or tracks), and a value. Complete the function using the rules below to modify the object passed to the function.
+
+Your function must always return the entire object.
+If prop isn't tracks and value isn't an empty string, update or set that album's prop to value.
+If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
+If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+If value is an empty string, delete the given prop property from the album.
+Note: A copy of the collection object is used for the tests. */
+
+var collection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// JS Code below stores a copy of the collection object before changes are made to it.
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+// updateRecords function
+function updateRecords(id, prop, value) {
+if (value === "") {
+  delete collection[id][prop];
+} else if (prop === "tracks") {
+  collection[id][prop] = collection[id][prop] || [];
+  collection[id][prop].push(value);
+} else {
+  collection[id][prop] = value;
+}
+  return collection;
+}
+
+//test data. 
+console.log(updateRecords(5439, 'artist', 'ABBA'));
+console.log(updateRecords(2468, 'tracks', 'free'));
+console.log(updateRecords(2548, 'artist', 'Bon Jovi'));
+console.log(updateRecords(1245, 'tracks', 'Addicted to love'));
+//success!
+
+
+/* //accessing nested arrays
 var myPlants = [
-  {//object inside an array
+  {
     type: "flowers",
     list: ["rose", "tulip", "dandelion"]
   },
@@ -11,7 +67,7 @@ var myPlants = [
 ];
 var thirdTree = myPlants[1].list[2];
 console.log(thirdTree); //birch
-
+ */
 /* //accessing nested objects
 var myStorage = {
   "desk": {
